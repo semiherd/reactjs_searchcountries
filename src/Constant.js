b0b47d766af2231d6da36f6dafacc13dd4056383
Semih Erdogan <82077230+semiherd@users.noreply.github.com>
2023-07-12 11:30:08 +0300

    Update Constant.js

--- src/Constant.js
+++ src/Constant.js
@@ -14,12 +14,5 @@ export const MODES= {
 export const CONTINENT_OPTIONS=[
 	'all','africa','americas','antarctic','asia','europe','oceania'
 ]
-export const ERROR= [
-	{id:'FETCH_ALL_FAILED',text:'Countries can not be fetched from the source.'},
-	{id:'MODE_UPDATE_FAILED',text:'Mode can not be updated.'},
-	{id:'DETAIL_NAVIGATION_FAILED',text:'Country Detail can not be loaded'},
-	{id:'SEARCH_FAILED',text: 'Keyword can not be searched.'},
-	{id: 'SEARCH_RESPONSE_NULL',text:'There is no data for the searched keyword.'}
-]
 export const URL_FETCHALL= 'https://restcountries.com/v3.1/all?status=true&fields=borders,cca3,capital,continents,currencies,flags,languages,name,population,region,subregion,tld'	
 export const URL_FETCHBYCOUNTRY= 'https://restcountries.com/v3.1/name'	
